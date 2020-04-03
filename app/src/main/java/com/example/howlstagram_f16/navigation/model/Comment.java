004b6a679c53a5aba00881bd94d475f8e76a282b
LeeChangHee <gjsxj725@daum.net>
2020-04-03 17:39:05 +0900

    commit!

---
+++ app/src/main/java/com/example/howlstagram_f16/navigation/model/Comment.java
@@ -0,0 +1,44 @@
+package com.example.howlstagram_f16.navigation.model;
+
+public class Comment {
+    public String uid = null;
+    public String userId = null;
+    public String comment = null;
+    public long timestamp = 0;
+
+    public Comment() {
+
+    }
+
+    public String getUid() {
+        return uid;
+    }
+
+    public void setUid(String uid) {
+        this.uid = uid;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    public String getComment() {
+        return comment;
+    }
+
+    public void setComment(String comment) {
+        this.comment = comment;
+    }
+
+    public long getTimestamp() {
+        return timestamp;
+    }
+
+    public void setTimestamp(long timestamp) {
+        this.timestamp = timestamp;
+    }
+}
